629a285fad5f2d194a4460cd5a44e5cf3a0ad13c
SergioFoma <s.kuzminsky.2007@gmail.com>
2025-08-30 22:05:51 +0300

    Do makefile and documentation

---
+++ Makefile
@@ -0,0 +1,33 @@
+CXX = g++
+
+BUILD_DIR = Build
+
+FLAGS = -Wshadow -Winit-self -Wredundant-decls -Wcast-align \
+	-Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations\
+	-Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain\
+	-Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy\
+	-Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wno-missing-field-initializers\
+	-Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing\
+	-Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -Werror=vla -D_DEBUG -D_EJUDGE_CLIENT_SIDE
+
+EXE_NAME = $(BUILD_DIR)/Result.exe
+
+SOURCES = stringFunction.cpp allFunction.cpp
+OBJECTS = $(SOURCES:.cpp=.obj) 
+
+OBJECTS_IN_SOURCE = $(addprefix src/, $(OBJECTS))
+
+.PHONY: all clean
+
+all: $(EXE_NAME)
+
+$(EXE_NAME): makeBuild $(OBJECTS_IN_SOURCE)
+	$(CXX) $(FLAGS) $(addprefix ./$(BUILD_DIR)/, $(OBJECTS_IN_SOURCE)) -o $(EXE_NAME)
+
+$(OBJECTS_IN_SOURCE): %.obj: %.cpp
+	$(CXX) $(FLAGS) -I ./include -c ./$^ -o ./$(BUILD_DIR)/$@ 
+
+makeBuild:
+	mkdir -p ./$(BUILD_DIR)/src
+clean:
+	rm -rf ./$(BUILD_DIR)
---
+++ src/allFunction.cpp
@@ -0,0 +1,191 @@
+#include "allFunction.h"
+
+#include <malloc.h>
+
+int myPuts( const char* line ) {
+    
+    if ( line == NULL ) {
+        return EOF;
+    }
+    
+    int index = 0;
+    
+    while ( line[ index ] != '\0' ) {
+        
+        putchar( line[ index++ ] );
+    }
+    
+    putchar('\n');
+    return 1;
+}
+
+char* myStrchr( const char* line, int ch ) {
+    
+    int index = 0;
+    
+    while( line[ index ] != '\0' ) {
+        
+        if ( line[ index ] == ch ) {
+            
+            return (char*)( &(line[ index ]) ) ;
+        }
+        
+        ++index;
+    }
+    
+    if ( line[ index ] == '\0' ) {
+        return NULL;
+    }
+}
+
+
+size_t myStrlen( const char* line ) {
+    
+    int index = 0;
+    
+    while ( line[ index ] != '\0' ) {
+        
+        ++index;
+    }
+    
+    return index;
+}
+
+char* myStrcpy( char* strDestination, const char *strSource ) {
+    
+    int index = 0;
+    
+    while( strSource[ index ] != '\0' ) {
+        strDestination[ index ] = strSource[ index ];
+        ++index;
+    }
+    
+    strDestination[ index ] = '\0';
+
+    return strDestination;
+}
+
+char* myStrncpy( char* strDest, const char* strSource, size_t count ) {
+
+    size_t index = 0;
+
+    while( index < count && strSource[ index ] != '\0' ) {
+
+        strDest[ index ] = strSource[ index ];
+        ++index;
+    }
+
+    strDest[ index ] = '\0';
+
+    return strDest;
+}
+
+char* myStrcat( char* strDestination, const char* strSource ) {
+
+    int indexDestination = 0;
+
+    for( ; strDestination[ indexDestination ] != '\0'; indexDestination++ );
+
+    for( int indexSource = 0; strSource[ indexSource ] != '\0'; indexSource++ ) {
+
+        strDestination[ indexDestination ] = strSource[ indexSource ];
+        ++indexDestination;
+    }
+
+    strDestination[ indexDestination ] = '\0';
+
+    return strDestination;
+}
+
+char* myStrncat( char* strDestination, const char* strSource, size_t count ) {
+
+    size_t indexDestination = 0;
+
+    for( ; strDestination[ indexDestination ] != '\0'; indexDestination++ );
+
+    for( size_t indexSource = 0; strSource[ indexSource ] != '\0' && indexSource < count; indexSource++ ) {
+
+        strDestination[ indexDestination ] = strSource[ indexSource ];
+        indexDestination++;
+    }
+
+    strDestination[ indexDestination ] = '\0';
+
+    return strDestination;
+
+}
+
+char* myFgets( char* str, int numChars, FILE* stream ) {
+
+    int index = 0, c = '0';
+
+    while ( index < numChars && ( c = fgetc( stream ) ) != EOF ) {
+
+        str[ index++ ] = c;
+    }
+
+    str[ index ] = '\0';
+
+    return str;
+    
+}
+
+char* myStrdup( const char* line ) {
+
+    char* copy = ( char* )calloc( myStrlen( line ), sizeof( char ) );
+
+    if ( copy == NULL ) {
+        return NULL;
+    }
+
+    int index = 0;
+
+    while( line[ index ] != '\0' ) {
+        copy[ index ] = line[ index ];
+        ++index;
+    }
+
+    copy[ index ] = line[ index ];
+
+    return copy;
+}
+
+ssize_t myGetline( char** line, size_t* n, FILE* stream ) {
+
+    if ( line == NULL ) {
+        return -1;
+    }
+
+    const size_t startSize = 32;
+
+    if ( *line == NULL ) {
+
+        *line = ( char* )calloc( startSize , sizeof( char ) );
+        *n = startSize;
+    }
+
+    int getChar = '0';
+    ssize_t index = 0;
+
+    while( ( getChar = fgetc( stream ) ) != '\n') {
+
+        if ( index  == ( *n - 1 ) ) {
+
+            char* tryAllocateMemory = ( char* )realloc( *line, *n * 2);
+
+            if ( tryAllocateMemory == NULL ) {
+                return -1;
+            }
+            else {
+                *line = tryAllocateMemory;
+            }
+            *n *= 2;
+        }
+
+        (*line)[ index++ ] = getChar;
+    }
+
+    ((*line)[ index ]) = '\0';
+
+    return index;
+}
